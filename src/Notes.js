0543a7d8b3b4d5fe490089c743f0eb19104df7f2
Sepehr Ronagh <sepehrronagh@hotmail.com>
2023-07-04 19:52:05 -0400

    Add edit for notes & empty notes field validation

--- src/Notes.js
+++ src/Notes.js
@@ -1,4 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { Tooltip } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Drawer from "@mui/material/Drawer";
@@ -11,6 +15,7 @@ import {
   doc,
   query,
   where,
+  updateDoc,
 } from "firebase/firestore";
 
 function Notes({ date }) {
@@ -18,6 +23,9 @@ function Notes({ date }) {
   const [openDrawer, setOpenDrawer] = useState(false);
   const [note, setNote] = useState("");
   const [userUid, setUserUid] = useState(null);
+  const [editingNoteId, setEditingNoteId] = useState(null);
+  const [noteError, setNoteError] = useState(false); // State variable for input validation
+  const notesInputRef = useRef(null); // Reference to the notes input TextField
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -63,13 +71,28 @@ function Notes({ date }) {
   };
 
   const submitNote = async () => {
+    if (!note) {
+      setNoteError(true); // Show validation error if note is empty
+      return;
+    }
+
     try {
       if (notesCollectionRef) {
-        await addDoc(notesCollectionRef, {
-          note: note,
-          date: date, // Add the date to the note
-        });
+        if (editingNoteId) {
+          const noteDoc = doc(notesCollectionRef, editingNoteId);
+          await updateDoc(noteDoc, {
+            note: note,
+            date: date, // Add the date to the note
+          });
+          setEditingNoteId(null);
+        } else {
+          await addDoc(notesCollectionRef, {
+            note: note,
+            date: date, // Add the date to the note
+          });
+        }
         setNote("");
+        setNoteError(false); // Reset validation error
         getNotes();
       }
     } catch (err) {
@@ -81,6 +104,20 @@ function Notes({ date }) {
     if (notesCollectionRef) {
       const noteDoc = doc(notesCollectionRef, id);
       await deleteDoc(noteDoc);
+    }
+  };
+
+  const editNote = (noteId) => {
+    const noteToEdit = newNote.find((note) => note.id === noteId);
+    if (noteToEdit) {
+      setEditingNoteId(noteToEdit.id);
+      setNote(noteToEdit.note);
+      setOpenDrawer(true);
+
+      // Set focus on the notes input TextField
+      if (notesInputRef.current) {
+        notesInputRef.current.focus();
+      }
     }
   };
 
@@ -101,19 +138,42 @@ function Notes({ date }) {
       </Button>
       <Drawer anchor="right" open={openDrawer} onClose={handleCloseDrawer}>
         <TextField
-          placeholder="Notes"
+          variant="outlined"
+          label="Notes"
           value={note}
           onChange={(e) => setNote(e.target.value)}
+          error={noteError} // Add error prop to TextField
+          helperText={noteError && "Please enter a note"} // Display helper text if there's a validation error
           rows={4}
           cols={50}
           style={{ resize: "none" }}
+          inputRef={notesInputRef} // Set the ref to the input TextField
         />
-        <Button onClick={submitNote}>Add note</Button>
+        <Button onClick={submitNote}>
+          {editingNoteId ? "Update Note" : "Add Note"}
+        </Button>
         <div>
           {newNote.map((note) => (
             <div key={note.id}>
               <h1>{note.note}</h1>
-              <Button onClick={() => deleteNote(note.id)}>Delete Note</Button>
+              <Tooltip title="Delete note">
+                <IconButton
+                  onClick={() => deleteNote(note.id)}
+                  aria-label="delete"
+                  sx={{ color: "red" }}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Edit Set">
+                <IconButton
+                  onClick={() => editNote(note.id)}
+                  aria-label="edit"
+                  sx={{ color: "green" }}
+                >
+                  <EditIcon />
+                </IconButton>
+              </Tooltip>
             </div>
           ))}
         </div>

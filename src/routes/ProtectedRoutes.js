cbd629c61c39c7618fafb6ce232db3d40adbbdbd
Sepehr Ronagh <sepehrronagh@hotmail.com>
2023-11-16 17:44:22 -0500

    Check auth state change on refresh

--- src/routes/ProtectedRoutes.js
+++ src/routes/ProtectedRoutes.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { auth } from "../firebase/firebaseConfig";
 
 function ProtectedRoutes() {
-  let user = auth?.currentUser?.email;
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  console.log(user);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      setUser(authUser);
+      setLoading(false);
+    });
+
+    // Cleanup function to unsubscribe from the auth state changes
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    // Loading indiciator while waiting for auth check
+    return <div>Loading...</div>;
+  }
 
   return user ? <Outlet /> : <Navigate to="/signin" />;
 }
